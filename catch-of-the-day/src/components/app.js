1b520312bf2b4e478f3838a05a8c83407d38d895
Amo Moloko <u14284783@tuks.co.za>
2018-06-11 13:59:15 +0200

    Explored State

--- catch-of-the-day/src/components/app.js
+++ catch-of-the-day/src/components/app.js
@@ -5,6 +5,20 @@ import Order from './Order';
 
 
 class App extends React.Component{
+    state = {
+        fishes:{},
+        order:{}
+    };
+
+    addFish = fish => {
+     //1. Take a copy of existing state
+     const fishes = {...this.state.fishes};
+     //2.Add a new fish to the copies
+     fishes[`fish${Date.now()}`] = fish;
+     //3.Set the new fishes object to state
+     this.setState({fishes: fishes })
+    };
+
     render(){
         return(
         
@@ -13,9 +27,9 @@ class App extends React.Component{
                <Header/>
 
             </div>
-           {<Order /> }
+           <Order /> 
   
-            { <Inventory/> }
+            <Inventory addFish={this.addFish}/> 
             </div>
 
         );
