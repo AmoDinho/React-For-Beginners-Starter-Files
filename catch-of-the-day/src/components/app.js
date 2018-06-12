bfc606792e886c950fc54f9294ee8404baa1c8b5
Amo Moloko <u14284783@tuks.co.za>
2018-06-12 12:02:06 +0200

    Add state functionaility for the orders

--- catch-of-the-day/src/components/app.js
+++ catch-of-the-day/src/components/app.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component{
     state = {
@@ -23,13 +24,28 @@ class App extends React.Component{
      this.setState({ fishes: sampleFishes});
     }
 
+    addToOrder = (key) => {
+        //1. take a copy of state
+        const order = {...this.state.order};
+        //2Either add or update order
+        order[key] = order[key] + 1||1;
+        //3.call set state to update our state object
+        this.setState({order});
+    }
+
     render(){
         return(
         
             <div className="catch-of-the-day">
                <div className="menu">
-               <Header/>
+               <Header tagline="Fresh Seafood Market"/>
+                <ul className="fishes">
+                {Object.keys(this.state.fishes).map(key => 
+                <Fish key={key} details={this.state.fishes[key]} 
+                index={key} 
+                addToOrder={this.addToOrder}/>)}
 
+                </ul>
             </div>
            <Order /> 
   
